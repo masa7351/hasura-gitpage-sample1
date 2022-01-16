f5ef03d78babccc030abff70e4c96d6900e6d997
masa7351 <masa7351@users.noreply.github.com>
2022-01-16 09:21:26 +0000

    deploy: ac147775f446be4143ea0f84b8b4e91b232b1acf

---
+++ _next/static/PYGNvfdCHaKOQDYJOoytM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/919-2f56379ae6c4ce6b.js","static/chunks/pages/index-79c6ed3ffcb16a6c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
