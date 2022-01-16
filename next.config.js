f02f169e3e6a85e23dc3bac4ac7fb2bbaed8a763
Masanao Imai <masa7351@gmail.com>
2022-01-16 18:37:28 +0900

    add prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // see: https://maku.blog/p/au8ju6g/
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
